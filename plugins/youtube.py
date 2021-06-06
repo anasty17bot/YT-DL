5ec895acaa1250342c6f44f40f164040600e5a20
HM-MODS <85007966+HM-MODS@users.noreply.github.com>
2021-06-06 13:08:28 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -50,7 +50,7 @@ async def ytdl(_, message):
     except Exception as e:
         print(e)
         try:
-            thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
+            thumbnail_url = "https://telegra.ph/file/0bf5ea7736e43bd63488e.png"
             await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
         except Exception as e:
             await sentm.edit(
